c2920f44c121ec59db2896e91665410536fcb605
Steve Pilcher <stevepilcheris@gmail.com>
2020-07-22 11:19:44 +0000

    Create DB connection & base,my recipe templates

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'recipe_manager'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster.djr3q.mongodb.net/recipe_manager?retryWrites=true&w=majority'
+
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/base')
+def index():
+    user = {'username': 'Steve'}
+    return render_template('base.html', title='Home', user=user)
+
+@app.route('/my_recipes')
+def my_recipes():
+    user = {'username': 'Tony'}
+    return render_template('myrecipes.html',
+    recipes=mongo.db.recipes.find(),
+    title='My Recipes', user=user)
+
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
+    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')),
     debug=True)
