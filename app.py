32e2bbb84bed3a6c361f931376d569f1be59270b
Steve Pilcher <stevepilcheris@gmail.com>
2020-07-23 13:57:58 +0000

    create template block and add image folder to static

--- app.py
+++ app.py
@@ -27,5 +27,6 @@ def my_recipes():
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')),
+    app.run(host=os.environ.get('IP'),
+    port=int(os.environ.get('PORT')),
     debug=True)
