4d62c74d15b1b3a26d4aa2962566cef81315a77c
Steve Pilcher <stevepilcheris@gmail.com>
2020-08-05 11:25:51 +0000

    Create login & signup functions

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for, flash, redirect
+from flask import Flask, render_template, redirect, request, session, url_for, flash, redirect
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_pymongo import PyMongo
+import bcrypt
 from bson.objectid import ObjectId
 from config import Config
 from flask_wtf import FlaskForm
@@ -19,16 +21,43 @@ app.config.from_object(Config)
 mongo = PyMongo(app)
 
 @app.route('/')
-@app.route('/index')
 def index():
-    user = {'username': 'Steve'}
-    return render_template('base.html', title='Home', user=user)
+    if 'username' in session:
+        return 'You are logged in as ' + session ['username']
+    return render_template('login.html', title='Home')
 
+@app.route('/login', methods=['POST'])
+def login():
+    users = mongo.db.users
+    login_user = users.find_one({'name' : request.form['username']})
+
+    if login_user:
+        if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password']) == login_user['password']:
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+
+    return 'Invalid username/password combination'
+
+
+@app.route('/signup', methods=['POST','GET'])
+def signup():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name' : request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+
+        return 'That username already exists!'
+
+    return render_template('signup.html')
 
 @app.route('/create_recipe')
 def create_recipe():
-    user = {'username': 'Steve'}
-    return render_template('createrecipe.html', title='Create Recipe', user=user,
+    return render_template('createrecipe.html', title='Create Recipe',
     categories=mongo.db.categories.find(),
     )
 
@@ -57,7 +86,7 @@ def edit_recipe(recipe_id):
     return render_template('editrecipe.html', recipe=the_recipe, categories=categories)
 
 
-@app.route('/update_recipe/<recipe_id>', methods=['POST', 'GET'])
+@app.route('/update_recipe/<recipe_id>', methods=['POST'])
 def update_recipe(recipe_id):
     recipes = mongo.db.recipes
     recipes.update({'_id': ObjectId(recipe_id)},
@@ -65,24 +94,15 @@ def update_recipe(recipe_id):
         'category_name':request.form.get('category_name'),
         'recipe_name':request.form.get('recipe_name'),
         'ingredients':request.form.get('ingredients'),
-        'preparaion_instructions':request.form.get('preparation_instructions'),
+        'preparation_instructions':request.form.get('preparation_instructions'),
         'cooking_instructions':request.form.get('cooking_instructions'),
         'prep_time':request.form.get('prep_time'),
         'cook_time':request.form.get('cook_time')
     })
     return redirect(url_for('my_recipes'))
 
-@app.route('/')
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', title='Sign In', form=form)
-
 if __name__ == '__main__':
+    app.secret_key= 'mysecret'
     app.run(host=os.environ.get('IP'),
     port=int(os.environ.get('PORT')),
     debug=True)
